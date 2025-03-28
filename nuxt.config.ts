805e76db75d7b0e3abcfb3bc8cae542b0981ecd8
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-03-28 23:32:58 +0100

    feat(styles): добавлен Tailwind CSS при помощи bun

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+  // Подключаем модули
+  modules: [
+    // ESLint для проверки кода
+    "@nuxt/eslint",
+  ],
+  // Подключаем CSS-файл
+  css: ["~/assets/css/main.css"],
+  // Подключаем плагин
+  vite: {
+    // Подключаем плагин tailwindcss
+    plugins: [tailwindcss()],
+  },
+});
