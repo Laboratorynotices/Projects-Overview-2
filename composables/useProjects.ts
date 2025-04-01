66caf5658f22cac2abfe58e96ff411587f9fb8c6
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-04-01 22:15:06 +0200

    feat(useProjects): загрузка данных от сервера в fetchProjects()
    Функция fetchProjects() теперь получает данные от сервера вместо локального источника.

--- composables/useProjects.ts
+++ composables/useProjects.ts
@@ -2,34 +2,12 @@ import { ref, computed } from "vue";
 import { type Project } from "~/types/project.types";
 
 export function useProjects() {
-  const projects = ref<Project[]>([
-    {
-      name: "Vogelsberg",
-      url: "https://google.com",
-      description:
-        "Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. ",
-      technologies: ["Vue.js", "html5"],
-    },
-    {
-      name: "Steinhaufen",
-      url: "https://google.de",
-      description:
-        "Aha-hu-ha ahu uha. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. Uhu-hu-hu uhu uhu. ",
-      technologies: ["cakephp", "html5"],
-    },
-    {
-      name: "Wassertahl",
-      url: "https://google.at",
-      description: "Tropf-Tropf, Tropf, Tropf, Tropf-Tropf. Tropf.",
-      technologies: ["yii2", "html5"],
-    },
-  ]);
+  const projects = ref<Project[]>([]);
 
-  // В будущем здесь будет метод для загрузки из API/БД
+  // Метод для загрузки из API/БД
   const fetchProjects = async () => {
     try {
-      // projects.value = await $fetch('/api/projects')
-      // Пока используем моковые данные
+      projects.value = await $fetch("/api/projects");
       return projects.value;
     } catch (error) {
       console.error("Ошибка загрузки проектов:", error);
