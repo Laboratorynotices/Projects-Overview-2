7e28a84acab60eda7f15970d08caaec13af2fb47
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-04-01 18:32:47 +0200

    feat(app): использование technologies из ~/composables/useProjects в app.vue

--- app.vue
+++ app.vue
@@ -15,46 +15,7 @@ useSeoMeta({
 });
 
 // Используем наш composable
-const { projects /*, technologies, fetchProjects */ } = useProjects();
-
-// Получение уникального списка технологий с использованием map() и reduce()
-const technologiesOld: string[] = projects.value
-  // Сначала проходим по массиву проектов и вытаскиваем из него массивы технологий
-  .map((project: Project) => {
-    // Вытягиваем массивы с технологиями из списка проектов.
-    return project.technologies;
-    // На выходе получаются двухмерные массивы технологий.
-  })
-  .reduce((accumulator: string[], current: string[]) => {
-    // Переделываем двухмерный массив в одномерный, но более длинный
-    return accumulator.concat(current);
-    // На выходе получается одномерный массив с технологиями, но значения могут быть двойными
-  }, [])
-  .reduce((accumulator: string[], current: string) => {
-    // Избавляемся от двойных значений в массиве
-
-    // Проверка есть ли это значение уже в результате
-    if (accumulator.includes(current)) {
-      // Да, нынешний элемент уже находится в списке.
-      // Ничего не делаем, просто возвращаем аккамулятор.
-      return accumulator;
-    } else {
-      // Этого значения ещё нет в списке,
-      // Добавляем его в список.
-      return [...accumulator, current];
-    }
-
-    // На выходе получается список технологий без повторений.
-  }, []);
-
-// Вычисляемое свойство для уникальных технологий
-const technologies: ComputedRef<string[]> = computed(() => {
-  return [
-    ...new Set(
-      projects.value.flatMap((project: Project) => project.technologies)
-    ),
-  ];
-});
+const { projects, technologies /*, fetchProjects */ } = useProjects();
 </script>
 
 <template>
